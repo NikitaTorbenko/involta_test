25ec03ac15be7fa5c062eefa4ed5a6e0eb3f84a2
NikitaTorbenko <2015nekitciti@gmail.com>
2025-10-01 13:47:03 +0300

    feat: add global settings

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import "~/assets/styles/reset.css";
+import "~/assets/styles/variables.css";
 </script>
 
 <template>
@@ -9,3 +10,12 @@ import "~/assets/styles/reset.css";
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+* {
+  font-family: Arial;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+}
+</style>
