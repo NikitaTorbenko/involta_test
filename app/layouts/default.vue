067fbd250eed4a98972150dbfd4987219ca05ed0
NikitaTorbenko <2015nekitciti@gmail.com>
2025-10-01 09:37:30 +0300

    init

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+// interface Props {
+
+// }
+</script>
+
+<template>
+  <div class="block">
+    <h2 class="title">default layout</h2>
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+// .block {
+// }
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+// interface Props {
+
+// }
+</script>
+
+<template>
+  <div class="block">
+    <h2 class="title">home page</h2>
+    <router-link to="/news">news</router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+// .block {
+// }
+</style>
